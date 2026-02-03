3f40524b6b2013798ed0cde362ba95d06c0bc65b
takayums <takayums20@gmail.com>
2026-02-03 17:34:46 +0700

    02-comment

---
+++ 02-comment.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  // This is Single Comment
+  std::cout << "This is Not Comment Code" << "\n";
+  /*
+   * This Multiple Comment
+   * Comment Line 2
+   * */
+  return 0;
+}
