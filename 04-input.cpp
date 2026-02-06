0537f9602f7dd37f8fd24904010d2de0f9888da8
takayums <takayums20@gmail.com>
2026-02-06 19:41:44 +0700

    04-input: input user with cli terminal

---
+++ 04-input.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+  // User Input With Cin
+  std::string name;
+  std::cout << "What is your Name: ";
+  std::cin >> name;
+  std::cout << "My Name is " << name << "\n";
+  return 0;
+}
